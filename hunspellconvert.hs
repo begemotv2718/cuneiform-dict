900cf6dc450a22c57f239f15ad6c1bd5657f5e31
begemotv2718 <begemotv2718@gmail.com>
2013-12-23 02:22:53 +0300

    drafts of hunspellconvert

---
+++ hunspellconvert.hs
@@ -0,0 +1,16 @@
+--What we have (baseform, [LetterIndex]) (dic file) + (LetterIndex, [AffRule]) (aff file). This is what hunspell provides
+--We will likely have the following additional file (word, freq)
+--Likely we will have the following intermediate form [(Stem, [Suffix])], and [(Stem,freq,[Suffix])]
+--We will need to convert [(Stem,freq,[Suffix])] into [(Stem,freq,SuffixIndex)]+Array SuffixIndex SuffixSet
+import Data.Array
+
+type LetterIdx = Char
+type LetterWord = [Letter]
+Data AffRule = Pfx { startGroup:: [Letter], replacement :: [Letter], match :: RegexRule } | Sfx { startGroup:: [Letter], replacement :: [Letter], match :: RegexRule  } | OtherAff
+Data RegexRule = [[Letter]]
+Data AffixMap = Array LetterIdx [AffRule] -- mapping between letter index and affix rule. This is a result of parsing aff file
+Data DicFileRec = DicFileRec { baseform:: LetterWord, modifiers:: [LetterIdx] } -- record in a dic file.
+Data WordNest = WordNest { stem:: [Letter], suffixes :: [[Letter]] } 
+Data AnnotatedNest = AnnotatedNest { wordnest :: WordNest, frequ :: Float }
+
+
